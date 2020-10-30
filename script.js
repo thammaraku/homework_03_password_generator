34f79104c89e71710be84ff6259bab1e0a8c0be7
thammaraku <thammaraku@gmail.com>
2020-10-30 14:29:17 -0500

    add user no input code

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
 var output = document.querySelector("#box4");
 var buttonEl = document.querySelector("#start");
-var combList = []
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var lowers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-// var specials = [~ ! @ # $ % ^ * - _ = + [ { ] } / ; : , . ?]
-
-
+// var goodbye = "Since you did not select any.\nGood Bye!"
 
 buttonEl.addEventListener("click", function() {
-
 var combList = []
 
+    function getRandomNumber() {
+        return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    }
 
+    function getRandomUpper() {
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
 
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+    function getRandomLower() {
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
 
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomSpecial() {
-    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
-}
-
+    function getRandomSpecial() {
+        return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+    }
 
 var password = "";
 var userTry = 0;
@@ -43,32 +34,20 @@ while (userTry < 3) {
         alert("Your input is not within range. Please input the number within range 8 - 128 characters");
         userTry++;
     } else {
-        alert("Your password will be " + length + " characters length");
+        // alert("Your password will be " + length + " characters length");
         var userTry = 99;
     }
     console.log(userTry);
 
     if (userTry === 3) {
         var length = 8;
-        alert("Since your selection was not within range. Your password will be 8 characters length");
-        var userTry = 0;
+        alert("Your selections were not within range. Your password will be forced to 8 characters length");
+        var userTry = 99;
     }
 }
 
 var userTry = 0;
 while (userTry < 2) {
-
-    // var pickNumber = confirm("Do you need number in your password?");
-    // console.log(pickNumber);
-
-    // var pickLower = confirm("Do you need lower case characters in your password?");
-    // console.log(pickLower);
-
-    // var pickUpper = confirm("Do you need upper case characters in your password?");
-    // console.log(pickUpper);
-
-    // var pickSpecial = confirm("Do you need special characters in your password?");
-    // console.log(pickSpecial);
 
     var pickNumber = confirm("Do you need number in your password?");
     if (pickNumber === true) {
@@ -94,71 +73,17 @@ while (userTry < 2) {
     }
     console.log(combList);
 
-
-    // var pickNumber = confirm("Do you need number in your password?");
-    // if (pickNumber === "true") {
-    //     combList.push("Number");
-    // }
-    // combList.push(confirm("Do you need number in your password?"));
-    // console.log(combList);
-
-    // combList.push(confirm("Do you need lower case characters in your password?"));
-    // console.log(combList);
-
-    // combList.push(confirm("Do you need upper case characters in your password?"));
-    // console.log(combList);
-
-    // combList.push(confirm("Do you need special characters in your password?"));
-    // console.log(combList);
-
-
-    // var comList = ["true","true","false","false"]
-
-    // var comListNew = [];
-
-    // for(var i = 0; i < comList.length; i++) {
-
-    //     if (comList[0] === "true") {
-    //         comListNew.push("Number");
-    //     } else if (comList[1] === "true") {
-    //         comListNew.push("Number");
-
-    //     } else if (comList[2] === "true") {
-    //         comListNew.push("Number");
-
-    //     } else if (comList[3] === "true") {
-    //         comListNew.push("Number");
-
-    //     }
-    // }
-
-    // console.log(comListNew);
-
-
-
-    if ((pickNumber == "false") && (pickLocal == "false") && (pickUpper == "false") && (pickSpecial == "false")) {
-        
-        alert("Please select at least one type of character for your password");
-        userTry++;
+    if (combList === undefined || combList.length == 0) {
+        alert("Please select at least one type of character for your password\nTry again.");
+        userTry = userTry + 1;
+        console.log(userTry);
 
     } else {
         var userTry = 99;
-        // console.log("length " + length);
-        // console.log("pickNumber " + pickNumber);
-        // console.log("lower " + pickLower);
-        // console.log("upper " + pickUpper);
-        // console.log("special " + pickSpecial);
-
         console.log(password.length);
 
         while (password.length < length) {
 
-            // var comList = ["Number", "Special"]
-            // var comList = ["Lower", "Upper"]
-
-
-
-            // comList.forEach(getRandom);
             for(var i = 0; i < combList.length; i++) {
 
                 function getRandom(combList) {
@@ -177,77 +102,16 @@ while (userTry < 2) {
                     console.log(password.length);
                 
             }
-            
-            // if ((pickNumber = "true") && (pickLower = "true") ) {
-
-            //     console.log(count);
-            //     var randomNumberIndex = Math.floor(Math.random() * numbers.length);
-            //     console.log(numbers[randomNumberIndex]);
-            //     var password = password.concat(numbers[randomNumberIndex]);
-            //     console.log(password);
-
-            //     console.log(count);
-            //     var randomLowerIndex = Math.floor(Math.random() * lowers.length);
-            //     console.log(lowers[randomLowerIndex]);
-            //     var password = password.concat(lowers[randomLowerIndex]);
-            //     console.log(password);
-
-            // }
-
-            // if ((pickNumberr = "true")) {
-
-            //     var password = password.concat(getRandomNumber());
-            //     console.log(password);
-            //     console.log(password.length);
-
-            // }
-
-            // if ((pickLowerr = "true")) {
-
-
-            //     var password = password.concat(getRandomLower());
-            //     console.log(password);
-            //     console.log(password.length);
-
-
-            // }
-
-            // if ((pickUpperr = "true")) {
-
-
-            //     var password = password.concat(getRandomUpper());
-            //     console.log(password);
-            //     console.log(password.length);
-
-
-            // }
-
-            // if ((pickSpecialr = "true")) {
- 
-            //     var password = password.concat(getRandomSpecial());
-            //     console.log(password);
-            //     console.log(password.length);
-
-
-            // }
         
         }
+        output.setAttribute("style", "border: solid");
+        buttonEl.setAttribute("style", "background-color: green");
+        output.setAttribute("style", "background-color: yellow");
+        output.textContent = password;
 
     }
-
-    // if (userTry === 3) {
-    //     var length = 8;
-    //     alert("Since you did not select any, your password will have all four character types");
-    //     var userTry = 99;
-    // }
-
 }
-
-
-output.setAttribute("style", "border: solid");
-buttonEl.setAttribute("style", "background-color: green");
-output.setAttribute("style", "background-color: yellow");
-output.textContent = password;
+output.textContent = "You did not select any.\n Good Bye!";
 
 });
 
